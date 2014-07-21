c97ab2e3c67af2f491d5d0afe170212abc22f9cd
Hugo Fonseca <hugo.fonseca@beubi.com>
2014-07-21 15:11:47 +0100

    fixed url

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,3 @@
 default['destination_paht'] = "/opt/customFirefoxProfile"
-default['git_url'] = "git@github.com:hugofonseca/customfirefoxprofile.git"
+default['git_url'] = "https://github.com/hugofonseca/customfirefoxprofile.git"
 default['revision'] = "master"
